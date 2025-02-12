e0496885aab20fb67aeccaa1784eb9bd36e28ac7
Arno Moelich <190516820+Arno-ATU@users.noreply.github.com>
2025-02-12 14:01:33 +0000

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,28 @@
+namespace GardenApp
+{
+    public partial class AppShell:Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Register routes for navigation
+            //Routing.RegisterRoute("login", typeof(Views.LoginPage));
+
+            // Plants routes
+            Routing.RegisterRoute("plants/indoor-plants", typeof(Views.PlantsPage));
+            Routing.RegisterRoute("plants/outdoor-plants", typeof(Views.PlantsPage));
+            Routing.RegisterRoute("plants/seasonal-plants", typeof(Views.PlantsPage));
+
+            // Tools routes
+            Routing.RegisterRoute("tools/hand-tools", typeof(Views.ToolsPage));
+            Routing.RegisterRoute("tools/power-tools", typeof(Views.ToolsPage));
+            Routing.RegisterRoute("tools/watering-tools", typeof(Views.ToolsPage));
+
+            // Garden Care routes
+            Routing.RegisterRoute("gardencare/fertilizers", typeof(Views.GardenCarePage));
+            Routing.RegisterRoute("gardencare/pest-control", typeof(Views.GardenCarePage));
+            Routing.RegisterRoute("gardencare/soil-and-mulch", typeof(Views.GardenCarePage));
+        }
+    }
+}
